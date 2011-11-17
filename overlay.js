c555a540bce7455fad5366436bbb283954dac268
Ram <ram@cloudflare.com>
2011-11-17 10:41:01 -0800

    Moving code into pages

---
+++ overlay.js
@@ -0,0 +1,23 @@
+/**
+    Overlay a grid on the current web page
+*/
+
+var overlay_container = document.createElement("div");
+    overlay_container.setAttribute("id", "both-grid-overlay");
+    
+document.getElementsByTagName("body")[0].appendChild(overlay_container);
+
+var grid_off_link = document.createElement("a");
+    grid_off_link.setAttribute("href", "javascript:void(0)");
+    grid_off_link.setAttribute("onclick", "javascript:document.getElementById('both-grid-overlay').style.display='none';");
+    grid_off_link.innerHTML = "Turn Off Grid";
+
+document.getElementById("both-grid-overlay").appendChild(grid_off_link);
+                
+var overlay_css = document.createElement("link");
+    overlay_css.setAttribute("rel", "stylesheet");
+    overlay_css.setAttribute("type", "text/css");
+    overlay_css.setAttribute("href", "style.css");
+    
+document.getElementsByTagName("head")[0].appendChild(overlay_css);
+    
---
+++ test.js
@@ -0,0 +1,4 @@
+var overlay_js = document.createElement('script');
+overlay_js.setAttribute("type","text/javascript");
+overlay_js.setAttribute("src", "overlay.js");
+document.getElementsByTagName("head")[0].appendChild(overlay_js);
